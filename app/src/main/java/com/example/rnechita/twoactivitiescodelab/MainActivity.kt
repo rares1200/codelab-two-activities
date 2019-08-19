3b940f94a61d3d794d8beb5b3b0f914fd9192afe
Rares NECHITA <rnechita@bitdefender.com>
2019-08-19 17:46:14 +0300

    Finished lyfecycle and state codelab

--- app/src/main/java/com/example/rnechita/twoactivitiescodelab/MainActivity.kt
+++ app/src/main/java/com/example/rnechita/twoactivitiescodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,11 +16,63 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_MESSAGE = "com.example.rnechita.twoactivitiescodelab.extra.MESSAGE"
         const val TEXT_REQUEST = 1
+        const val REPLY_VISIBLE_KEY = "reply_visible"
+        const val REPLY_TEXT_KEY = "reply_text"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "-------")
+        Log.d(TAG, "onCreate")
+
+        savedInstanceState?.let {
+            val isVisible = it.getBoolean(REPLY_VISIBLE_KEY)
+            val reply = it.getString(REPLY_TEXT_KEY)
+            if (isVisible) {
+                text_header_reply.visibility = View.VISIBLE
+                text_message_reply.text = reply
+                text_message_reply.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        if (text_header_reply.visibility == View.VISIBLE) {
+            outState?.putBoolean(REPLY_VISIBLE_KEY, true)
+            outState?.putString(REPLY_TEXT_KEY, text_message_reply.text.toString())
+        }
     }
 
     fun launchSecondActivity(view: View) {
--- app/src/main/java/com/example/rnechita/twoactivitiescodelab/SecondActivity.kt
+++ app/src/main/java/com/example/rnechita/twoactivitiescodelab/SecondActivity.kt
@@ -4,10 +4,13 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
+    private val TAG = SecondActivity::class.java.simpleName
 
     companion object {
         const val EXTRA_REPLY = "com.example.rnechita.twoactivitiescodelab.extra.REPLY"
@@ -29,6 +32,7 @@ class SecondActivity : AppCompatActivity() {
         val replyIntent = Intent()
         replyIntent.putExtra(EXTRA_REPLY, reply)
         setResult(Activity.RESULT_OK, replyIntent)
+        Log.d(TAG, "End SecondActivity")
         finish()
     }
 }
